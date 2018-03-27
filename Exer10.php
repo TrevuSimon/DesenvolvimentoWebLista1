1f5653c1717c896091102c1144da1abc9e0ff78c
TADS <tads1@fapi.local>
2018-03-26 22:19:38 -0300

    Exercicio 10 OK

---
+++ Exer10.php
@@ -0,0 +1,18 @@
+<?php
+$mes = array(
+                "Janeiro",
+                "Fevereiro",
+                "Março",
+                "Abril",
+                "Maio",
+                "Junho",
+                "Julho",
+                "Agosto",
+                "Setembro",
+                "Outubro",
+                "Novembro",
+                "Dezembro"
+                );
+  $escolha = 10;
+  echo $mes[$escolha];
+ ?>
